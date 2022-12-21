3c8886acc7a9bd475393edee757a7c59218eb749
0K00 <qdouvillez@gmail.com>
2022-12-21 19:08:41 +0100

    :sparkles: Add handle error support

--- 01_Src/01_Client/src/services/api/api.ts
+++ 01_Src/01_Client/src/services/api/api.ts
@@ -1,3 +1,15 @@
+/*
+ * — Author: @qdouvillez
+ * — Create Time: 19 Dec 2022 at 13:25
+ * — Description: Api Service
+*/
+
+interface ApiError {
+    message: string;
+    statusCode: number;
+    action?: string;
+}
+
 class Api {
     private transQueries (q: Array<string>): String {
         let ret = "?";
@@ -7,6 +19,21 @@ class Api {
         }
         return ret;
     };
+
+    private redirect() {
+        return "hello"
+    }
+
+    private handleError(error: any): ApiError {
+        // Check the error status code and return a custom error object
+    if (error.status === 401) {
+        return { message: 'Unauthorized', statusCode: 401, action: this.redirect() };
+    } else if (error.status === 404) {
+        return { message: 'Not found', statusCode: 404 };
+    } else {
+        return { message: 'An unexpected error occurred', statusCode: 500 };
+    }
+    }
 
     public json(method: string, url: string, params: Array<string>, queries: Array<string>): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -24,12 +51,12 @@ class Api {
                 })
                 .catch((err) => {
                     console.error(err);
-                    return reject();
+                    return reject(this.handleError(err));
                 })
             })
             .catch((err) => {
                 console.error(err)
-                return reject();
+                return reject(this.handleError(err));
             })
         })
     }
