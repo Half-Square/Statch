49b37e8c4086c00fd3951cc0cf82c80bd76b3da4
0K00 <qdouvillez@gmail.com>
2022-12-21 19:08:15 +0100

    :recycle: Button component with Icon and option support

--- 01_Src/01_Client/src/components/button/Button.vue
+++ 01_Src/01_Client/src/components/button/Button.vue
@@ -1,41 +1,118 @@
 <!--
-Author – @0K00
-Create Time – 2022-10-15 14:17
-Description – Button component
+
+  File: Button.vue
+
+  Description: Button Component with Callback and Options support
+
+  Author: @qdouvillez
+
+  Date: 21 Dec 2022 at 15:08
+
+  Last Modified: 21 Dec 2022 at 15:08
+
+  Copyright: Copyright 2023, ©Half Square
+
+
+
+  Purpose:
+
+  -
+
+
+
+  Dependencies:
+
+  - button.scss: Style file
+
+
+
+  Notes:
+
+  -
+
+
+
+  Summary:
+
+  -
+
 -->
 
-<script lang="ts">
-  import { defineComponent } from "vue";
-  import './button.scss';
 
-  export default defineComponent({
-    name: 'Button',
+<script lang='ts'>
+    import { defineComponent } from 'vue';
+    import './button.scss';
 
-    props: {
-      label: {
-        type: String,
-        required: true
-      },
-      type: {
-        type: String,
-        required: true
-      },
-      id: {
-        type: String,
-        required: true
-      },
-    },
+    export default defineComponent({
+        name: 'Button',
 
-    computed: {
-      classes() {
-        return {
-          btn: true,
-        };
-      }
-    },
-  });
+        props: {
+            label: {
+                type: String,
+                required: false,
+            },
+            btn: {
+                type: String,
+                default: 'btn',
+            },
+            type: {
+                type: String,
+                default: 'prm',
+                validator(value: string) {
+                    return ['prm', 'scnd', 'trd', 'fth'].includes(value);
+                },
+            },
+            size: {
+                type: String,
+                default: 'md',
+                validator(value: string) {
+                    return ['sm', 'md', 'l', 'xl'].includes(value);
+                },
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            callback: {
+                type: Function as () => void,
+                required: true,
+            },
+            icon: {
+              type: String,
+              default: null
+            },
+            iconPosition: {
+              type: String,
+              default: 'left',
+              validator: (value: string) => ['left', 'right', 'none', 'only'].includes(value)
+            },
+        },
+
+        computed: {},
+
+        data() {
+            return {}
+        },
+
+        methods: {}
+    });
 </script>
 
 <template>
-  <button :type='type' :id='id' :class='classes'>{{ label }}</button>
+  <button @click="callback" :disabled="disabled" :class="[btn, type, size]" :id="id">
+    <template v-if="iconPosition === 'left'">
+      <i :class="icon"></i>
+      {{ label }}
+    </template>
+    <template v-if="iconPosition === 'right'">
+      {{ label }}
+      <i :class="icon"></i>
+    </template>
+    <template v-if="iconPosition === 'none'">
+      {{ label }}
+    </template>
+    <template v-if="iconPosition === 'only'">
+      <i :class="icon"></i>
+    </template>
+  </button>
 </template>
