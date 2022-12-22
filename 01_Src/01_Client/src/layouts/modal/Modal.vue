ba8deff3997cf9973deffaa4f42a9de79f16cd42
0K00 <qdouvillez@gmail.com>
2022-12-22 19:01:54 +0100

    :sparkles: New Compositions 'Modal' with hidden/show support

---
+++ 01_Src/01_Client/src/layouts/modal/Modal.vue
@@ -0,0 +1,74 @@
+<!--
+
+  File: Modal.vue
+
+  Description: Modal Component
+
+  Author: @qdouvillez
+
+  Date: 22 Dec 2022 at 16:08
+
+  Last Modified: 22 Dec 2022 at 16:08
+
+  Copyright: Copyright 2023, ©Half Square
+
+
+
+  Purpose:
+
+  -
+
+
+
+  Dependencies:
+
+  - .scss:
+
+
+
+  Notes:
+
+  -
+
+
+
+  Summary:
+
+  -
+
+-->
+
+
+<script lang='ts'>
+    import { defineComponent } from 'vue';
+    import './modal.scss';
+
+    export default defineComponent({
+        name: 'Modal',
+
+        props: ['show'],
+
+        computed: {},
+
+        data() {
+            return {
+              isModalVisible: false,
+            }
+        },
+
+        methods: {
+            closeModal() {
+              this.$emit('closeModal', false)
+              this.isModalVisible = true;
+            },
+        }
+    });
+</script>
+
+<template>
+  <div class="modal" v-if="!isModalVisible" @click="closeModal" @keydown.esc="closeModal">
+    <div class="modal-content" @click.stop>
+        <slot></slot>
+    </div>
+  </div>
+</template>
