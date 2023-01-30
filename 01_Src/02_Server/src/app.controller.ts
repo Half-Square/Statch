bf0c4e9f62a7dfa3e119482491676501613a99d0
Jbristhuille <jbristhuille@gmail.com>
2023-01-30 17:04:52 +0100

    :sparkles: Setup prisma and write example

--- 01_Src/02_Server/src/app.controller.ts
+++ 01_Src/02_Server/src/app.controller.ts
@@ -1,5 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get('')
+  async home(): Promise<any> {
+    const res = await this.prisma.project.findMany();
+    return res;
+  }
+
+  @Post('')
+  async createProject(): Promise<any> {
+    const res = await this.prisma.project.create({
+      data: {
+        name: 'test',
+        status: 'new',
+        version: '0.1.0',
+        created: undefined,
+        description: 'new project in database',
+      },
+    });
+
+    return res;
+  }
 }
