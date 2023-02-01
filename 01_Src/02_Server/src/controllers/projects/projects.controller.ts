4a3ad8601c692f8f01c0f857ea9b6ae9d8572252
Jbristhuille <jbristhuille@gmail.com>
2023-02-01 09:02:53 +0100

    :goal_net: Add try/catch on projects controller

--- 01_Src/02_Server/src/controllers/projects/projects.controller.ts
+++ 01_Src/02_Server/src/controllers/projects/projects.controller.ts
@@ -1,54 +1,90 @@
-import { Controller, Get, Post, Param, Body, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import * as projectsDto from '../../dto/projects.dto';
 import * as commentsDto from '../../dto/comments.dto';
 
 @Controller('projects')
 export class ProjectsController {
-    constructor(private prisma: PrismaService) {
-    }
+  constructor(private prisma: PrismaService) {}
 
-    @Get('')
-    async getAll(): Promise<projectsDto.publicOutput[]> {
+  @Get('')
+  async getAll(): Promise<projectsDto.publicOutput[]> {
+    try {
       const res = await this.prisma.project.findMany();
       return res.map((el) => new projectsDto.publicOutput(el));
+    } catch (err) {
+      console.error(`${new Date().toISOString()} - ${err}`);
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-  
-    @Get('/:id') // to do: 404 not found
-    async getOne(@Param('id') id: string): Promise<projectsDto.detailsOutput> {
-        const res = await this.prisma.project.findUnique({
-          where: {
-            id: Number(id)
-          }, 
-          include: {
-            comments: true
-          }
-        });
-        if (res) return new projectsDto.detailsOutput(res);
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-      }
+  }
 
-    @Post('')
-    async create(@Body() body: projectsDto.createInput): Promise<projectsDto.detailsOutput> {
-      const res = await this.prisma.project.create({data: body});
+  @Get('/:id')
+  async getOne(@Param('id') id: string): Promise<projectsDto.detailsOutput> {
+    try {
+      const res = await this.prisma.project.findUnique({
+        where: {
+          id: Number(id),
+        },
+        include: {
+          comments: true,
+        },
+      });
+      if (res) return new projectsDto.detailsOutput(res);
+      else throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    } catch (err) {
+      console.error(`${new Date().toISOString()} - ${err}`);
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Post('')
+  async create(@Body() body: projectsDto.createInput): Promise<projectsDto.detailsOutput> {
+    try {
+      const res = await this.prisma.project.create({ data: body });
       return new projectsDto.detailsOutput(res);
+    } catch (err) {
+      console.error(`${new Date().toISOString()} - ${err}`);
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    @Get('/:id/comments')
-    async getComments(@Param('id') id: string): Promise<commentsDto.publicOutput[]> {
-      const res = await this.prisma.comment.findMany({where: {parentId: Number(id)}});
+  @Get('/:id/comments')
+  async getComments(@Param('id') id: string): Promise<commentsDto.publicOutput[]> {
+    try {
+      const res = await this.prisma.comment.findMany({
+        where: { parentId: Number(id) },
+      });
       return res.map((el) => new commentsDto.publicOutput(el));
+    } catch (err) {
+      console.error(`${new Date().toISOString()} - ${err}`);
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    @Post('/:id/comments')
-    async addComment( @Param("id") id: string,
-                      @Body() body: commentsDto.createInput): Promise<any> {
+  @Post('/:id/comments')
+  async addComment( @Param('id') id: string,
+                    @Body() body: commentsDto.createInput): Promise<commentsDto.detailsOutput> {
+    try {
       const res = await this.prisma.comment.create({
         data: {
           content: body.content,
-          parentId: Number(id)
+          parentId: Number(id),
         },
       });
-      return res;
+      return new commentsDto.detailsOutput(res);
+    } catch (err) {
+      if (err.code == 'P2003')
+        throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+      else
+        throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 }
