f50b4f269771166d815353b4e3fe6d2adf439c97
0K00 <qdouvillez@gmail.com>
2023-02-23 17:22:14 +0100

    :art: Update Login's view for input component

--- 01_Src/01_Client/src/views/login/Login.vue
+++ 01_Src/01_Client/src/views/login/Login.vue
@@ -1,41 +1,9 @@
 <!--
-
-  File: Login.vue
-
-  Description: Login page
-
-  Author: @qdouvillez
-
-  Date: 24 Dec 2022 at 14:52
-
-  Last Modified: 24 Dec 2022 at 14:52
-
-  Copyright: Copyright 2023, ©Half Square
-
-
-
-  Purpose:
-
-  -
-
-
-
-  Dependencies:
-
-  - login.scss: Style file
-
-
-
-  Notes:
-
-  -
-
-
-
-  Summary:
-
-  -
-
+* @Author                : 0K00<qdouvillez@gmail.com>                         
+* @CreatedDate           : Invalid Date                                       
+* @LastEditors           : 0K00<qdouvillez@gmail.com>                         
+* @LastEditDate          : 2023-02-23 14:35:37                                
+*                                                                             
 -->
 
 
@@ -72,9 +40,10 @@
 
 <template>
     <div id="login">
+      <img src="@/assets/logotype.svg" alt="Statch logotype" width="102" height="28">
       <div class="container">
-        <Input id="mail" placeholder="Enter your email..." type="prm" size="md" />
-        <Input id="pwd" is="password" placeholder="Enter your password..." type="prm" size="md" />
+        <Input id="mail" placeholder="Enter your email..." type="prm" size="md" other="full" />
+        <Input id="pwd" is="password" placeholder="Enter your password..." type="prm" size="md" other="full" />
         <Button id='btnLogin' type='prm' size='md' label='Open' @click='login' icon='enter' iconPosition='right' />
         <a href="/create">
           <div>
