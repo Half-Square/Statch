056425f09db21c78f919c0b13310e0504638b3d9
0K00 <qdouvillez@gmail.com>
2023-02-24 17:37:53 +0100

    :sparkles: Add Nav's component

---
+++ 01_Src/01_Client/src/compositions/nav/Nav.vue
@@ -0,0 +1,26 @@
+<!--
+* @Author                : 0K00<qdouvillez@gmail.com>                         
+* @CreatedDate           : 2023-02-24 16:00:26                                
+* @LastEditors           : 0K00<qdouvillez@gmail.com>                         
+* @LastEditDate          : 2023-02-24 17:18:09                                
+*                                                                             
+-->
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import './nav.scss';
+
+    export default defineComponent({
+      name: 'Nav'
+    });
+
+    // team name
+    // navigation (projects, activity, add, follow, setting)
+    // menu project
+</script>
+
+<template>
+  <div id="nav">
+    Nav component work!
+  </div>
+</template>
