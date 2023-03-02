175f9c383084546823bbadcabdbff82209561052
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-03-02 15:45:40 +0100

    :sparkles: Enable cors

--- 01_Src/02_Server/src/main.ts
+++ 01_Src/02_Server/src/main.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-02-21 14:10:15                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-02-21 16:33:43                               *
+ * @LastEditDate          : 2023-03-02 15:45:17                               *
  *****************************************************************************/
 
 /* SUMMARY
@@ -18,7 +18,7 @@ import {AppModule} from "./app.module";
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
-
+  app.enableCors();
   await app.listen(process.env.PORT);
   console.log(
     `${new Date().toISOString()} - Server started on port ${process.env.PORT}`,
