e1e0f4db1e57c1d359a9e60243c964f04334fd3b
0K00 <qdouvillez@gmail.com>
2023-03-25 17:32:25 +0100

    :lipstick: Update avatar component

--- 01_Src/01_Client/src/app/components/avatar/avatar.component.ts
+++ 01_Src/01_Client/src/app/components/avatar/avatar.component.ts
@@ -1,22 +1,27 @@
-/******************************************************************************
- * @Author                : Adrien Lanco<adrienlanco0@gmail.com>              *
- * @CreatedDate           : 2023-03-17 12:11:36                               *
- * @LastEditors           : Adrien Lanco<adrienlanco0@gmail.com>              *
- * @LastEditDate          : 2023-03-17 12:13:43                               *
- *****************************************************************************/
+/*****************************************************************************
+ * @Author                : 0K00<qdouvillez@gmail.com>                       *
+ * @CreatedDate           : 2023-03-17 12:11:36                              *
+ * @LastEditors           : 0K00<qdouvillez@gmail.com>                       *
+ * @LastEditDate          : 2023-03-25 17:27:13                              *
+ ****************************************************************************/
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss']
 })
-export class AvatarComponent {
+export class AvatarComponent implements OnInit {
   @Input() userId: string = "";
 
   @Input() imgPath: string = "";
   @Input() alt: string = "";
+  @Input() userName: string = "";
 
   public classes: string = "";
+
+  ngOnInit() {
+    this.userName = this.userName.charAt(0);
+  }
 }
