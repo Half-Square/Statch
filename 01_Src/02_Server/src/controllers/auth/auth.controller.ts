e4c1fd49f712d8d54d373056d37a314b13370b37
Adrien Lanco <adrienlanco0@gmail.com>
2023-03-30 10:56:12 +0200

    :sparkles:  Creating admin guard

--- 01_Src/02_Server/src/controllers/auth/auth.controller.ts
+++ 01_Src/02_Server/src/controllers/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 /******************************************************************************
- * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
+ * @Author                : Adrien Lanco<adrienlanco0@gmail.com>              *
  * @CreatedDate           : 2023-02-21 13:01:19                               *
- * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-02-23 15:01:55                               *
+ * @LastEditors           : Adrien Lanco<adrienlanco0@gmail.com>              *
+ * @LastEditDate          : 2023-03-30 10:15:56                               *
  *****************************************************************************/
 
 /* SUMMARY
@@ -36,6 +36,7 @@ import * as jwt from "jsonwebtoken";
 /* Dto */
 import * as usersDto from "../../dto/users.dto";
 import { ConnectedGuard } from "../../guards/connected/connected.guard";
+import { IsAdminGuard } from "../../guards/is-admin/is-admin.guard";
 /***/
 
 /* Services */
@@ -81,6 +82,17 @@ export class AuthController {
   }
   /***/
 
+   /**
+  * Get one user
+  * @returns - User's details
+  */
+  @Get("isadmin")
+  @UseGuards(IsAdminGuard)
+  async isAdmin(): Promise<boolean> {
+      return true
+  }
+  /***/
+
   /**
   * Register new user 
   * @param name - User's name
@@ -92,11 +104,15 @@ export class AuthController {
   async register(@Body() body: usersDto.RegisterInput): Promise<usersDto.DetailsOutput> {
     try {
       let passwd = String(sha256(body.password));
+      let count = await (await this.prisma.user.findMany()).length
+      
       const res = await this.prisma.user.create({
         data: {
           name: body.name,
           password: passwd,
-          email: body.email
+          email: body.email,
+          validate: count === 0,
+          isAdmin: count === 0 
         }
       });
       return new usersDto.DetailsOutput(res);
@@ -123,9 +139,12 @@ export class AuthController {
       const res = await this.prisma.user.findUnique({where: {email: body.email}});
       if (!res) throw new HttpException("Not Found", HttpStatus.NOT_FOUND);
       
+
       if (res.password !== sha256(body.password)) {
         throw new HttpException("Invalid Password", HttpStatus.BAD_REQUEST);
       }
+
+      if (!res.validate) throw new HttpException("Not validate yet", HttpStatus.BAD_REQUEST);
 
       delete res.password;
       
@@ -142,17 +161,38 @@ export class AuthController {
   }
   /***/
 
+  // /**
+  // * Edit user
+  // * @returns - User's details
+  // */
+  // @Put("users/:id")
+  // @UseGuards(ConnectedGuard)
+  // async editUser(@Param("id") id: string, @Body() body: usersDto.UpdateInput,): Promise<usersDto.DetailsOutput> {
+  //   try {
+  //     const res = await this.prisma.user.update({
+  //       data: {validate: true},
+  //       where: {id: id} 
+  //     });
+
+  //     return new usersDto.DetailsOutput(res);
+  //   } catch (err) {
+  //     console.error(`${new Date().toISOString()} - ${err}`);
+  //     throw err;
+  //   }
+  // }
+  // /***/
+
   /**
   * Activate user
   * @returns - User's details
   */
-  @Put("users/:id")
-  @UseGuards(ConnectedGuard)
-  async activate(@Param("id") id: string): Promise<usersDto.DetailsOutput> {
+  @Put("users/:id/right")
+  @UseGuards(IsAdminGuard)
+  async activate(@Param("id") id: string,  @Body() body: usersDto.RightInput,): Promise<usersDto.DetailsOutput> {
     try {
       const res = await this.prisma.user.update({
-        data: {validate: true},
-        where: {id: id} 
+        where: {id: id},
+        data: body
       });
 
       return new usersDto.DetailsOutput(res);
