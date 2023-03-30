b9a7d7ce7430854c68a91ff9f498aea9a5550391
Adrien Lanco <adrienlanco0@gmail.com>
2023-03-30 14:53:21 +0200

    :card_file_box: SQLite migration

--- 01_Src/02_Server/prisma/migrations/20230323140513_test_versions/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `Version` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `version` ADD COLUMN `name` VARCHAR(191) NOT NULL;
--- 01_Src/02_Server/prisma/migrations/20230328171343_raz_pc_perso/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `level` on the `project` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `project` DROP COLUMN `level`;
