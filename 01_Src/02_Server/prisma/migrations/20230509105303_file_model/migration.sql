d54f9bea6a8ddc1c3abb30d6bdd45bd153ef9d3a
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-05-30 11:50:41 +0200

    :fire: Remove old code

--- 01_Src/02_Server/prisma/migrations/20230509105303_file_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "File" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
-    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- 01_Src/02_Server/src/guards/is-self/is-self.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IsSelfGuard } from './is-self.guard';
-
-describe('IsSelfGuard', () => {
-  it('should be defined', () => {
-    expect(new IsSelfGuard()).toBeDefined();
-  });
-});
