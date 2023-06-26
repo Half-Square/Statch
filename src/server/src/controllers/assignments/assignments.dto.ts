0fd9bb16fc46389db32593a9a78074fdc17b97ee
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-06-26 14:45:41 +0200

    :rotating_light: Fix lint and prisma schema indent

--- src/server/src/controllers/assignments/assignments.dto.ts
+++ src/server/src/controllers/assignments/assignments.dto.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-06-24 17:56:12                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-06-24 18:12:49                               *
+ * @LastEditDate          : 2023-06-26 14:44:47                               *
  *****************************************************************************/
 
 /* SUMMARY
@@ -11,7 +11,7 @@
 */
 
 /* Imports */
-import { IsArray, IsString } from "class-validator";
+import { IsArray } from "class-validator";
 /***/
 
 /**
