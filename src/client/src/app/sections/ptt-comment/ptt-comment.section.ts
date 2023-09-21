0a20933a3e65726e2aeeb020a2ed70c006a6acde
0K00 <qdouvillez@gmail.com>
2023-09-21 17:06:01 +0200

    :construction: PTT Comment section

---
+++ src/client/src/app/sections/ptt-comment/ptt-comment.section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'section-ptt-comment',
+  templateUrl: './ptt-comment.section.html',
+  styleUrls: ['./ptt-comment.section.scss']
+})
+export class PttCommentSection {
+
+}
