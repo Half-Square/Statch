05c91debb1edec0ed6906950fb43a0c4bbff5207
0K00 <qdouvillez@gmail.com>
2023-09-21 12:52:38 +0200

    :sparkles: Type guard for PTT routing & 404

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-/******************************************************************************
- * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @CreatedDate           : 2023-05-31 15:03:46                               *
- * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-06-02 15:16:16                               *
- *****************************************************************************/
+/*****************************************************************************
+ * @Author                : 0K00<qdouvillez@gmail.com>                       *
+ * @CreatedDate           : 2023-05-31 15:03:46                              *
+ * @LastEditors           : 0K00<qdouvillez@gmail.com>                       *
+ * @LastEditDate          : 2023-09-21 12:40:37                              *
+ ****************************************************************************/
 
 /* SUMMARY
   * Imports
@@ -14,17 +14,25 @@
 /* Imports */
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { TypeGuard } from "./services/guards/type-guard.service";
 /***/
 
 /* Views */
 import { LoginView } from "./views/login/login.view";
+import { NotFoundView } from "./views/not-found/not-found.view";
+import { PttView } from "./views/ptt/ptt.view";
 import { SignupView } from "./views/signup/signup.view";
 /***/
 
 /* Routes */
 const routes: Routes = [
   { path: "login", component: LoginView },
-  { path: "signup", component: SignupView }
+  { path: "signup", component: SignupView },
+
+  { path: ":type/:id", component: PttView, canActivate: [TypeGuard] },
+
+  { path: "not-found", component: NotFoundView },
+  { path: "*", redirectTo: "not-found" }
 ];
 /***/
 
---
+++ src/client/src/app/views/not-found/not-found.view.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'view-not-found',
+  templateUrl: './not-found.view.html',
+  styleUrls: ['./not-found.view.scss']
+})
+export class NotFoundView {
+
+}
