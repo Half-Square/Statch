4f1837dca8730cb67138cf64748679ad42116c55
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-09-22 18:37:37 +0200

    :bug: Fix connection guards, use user service

--- src/client/src/app/guards/is-connected.guard.ts
+++ src/client/src/app/guards/is-connected.guard.ts
@@ -2,11 +2,12 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-09-22 18:17:29                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-09-22 18:21:25                               *
+ * @LastEditDate          : 2023-09-22 18:35:59                               *
  *****************************************************************************/
 
 /* SUMMARY
   * Imports
+  * Services
 */
 
 /* Imports */
@@ -14,17 +15,20 @@ import { Injectable } from "@angular/core";
 import { Router, UrlTree } from "@angular/router";
 /***/
 
+/* Services */
+import { UserService } from "../services/user.service";
+/***/
+
 @Injectable({
   providedIn: "root"
 })
 export class IsConnectedGuard {
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private user: UserService) {
   }
 
   canActivate(): boolean | UrlTree {
-    let session = localStorage.getItem("user");
-
-    if (!session) this.router.navigateByUrl("/login");
+    if (!this.user.isConnected()) this.router.navigateByUrl("/login");
     return true;
   }
 }
--- src/client/src/app/guards/is-not-connected.guard.ts
+++ src/client/src/app/guards/is-not-connected.guard.ts
@@ -2,11 +2,12 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-09-22 18:23:52                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-09-22 18:25:03                               *
+ * @LastEditDate          : 2023-09-22 18:36:49                               *
  *****************************************************************************/
 
 /* SUMMARY
   * Imports
+  * Services
 */
 
 /* Imports */
@@ -14,16 +15,19 @@ import { Injectable } from "@angular/core";
 import { Router, UrlTree } from "@angular/router";
 /***/
 
+/* Services */
+import { UserService } from "../services/user.service";
+/***/
+
 @Injectable({
   providedIn: "root"
 })
 export class IsNotConnectedGuard {
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private user: UserService) {
   }
   canActivate(): boolean | UrlTree {
-    let session = localStorage.getItem("user");
-
-    if (session) this.router.navigateByUrl("/home");
+    if (this.user.isConnected()) this.router.navigateByUrl("/home");
     return true;
   }
 }
