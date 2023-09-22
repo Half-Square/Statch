ac7652a4b268ffa38a1b5928e2d54ea3aa2f6aba
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-09-22 16:12:01 +0200

    :goal_net: Update request service to implement error catching

--- src/client/src/app/services/request.service.ts
+++ src/client/src/app/services/request.service.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>        *
  * @CreatedDate           : 2023-05-30 16:14:10                              *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>        *
- * @LastEditDate          : 2023-09-20 16:43:14                              *
+ * @LastEditDate          : 2023-09-22 16:10:50                              *
  ****************************************************************************/
 
 /* SUMMARY
@@ -44,8 +44,12 @@ export class RequestService {
         headers: headers,
         body: body ? JSON.stringify(body) : null
       }).then((ret) => {
+        if (!(/2[0-9]{2}/g).test(String(ret.status))) throw ret.statusText;
+
         ret.json().then((data: unknown) => {
           return resolve(data);
+        }).catch((err) => {
+          return reject(err);
         });
       }).catch((err: Error) => {
         return reject(err);
