428615cd586c6a0a49a7e27a2e08df79c5ee4d99
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-09-25 13:29:11 +0200

    :sparkles: Implement create project button on projects view

--- src/client/src/app/views/projects/projects.view.ts
+++ src/client/src/app/views/projects/projects.view.ts
@@ -2,26 +2,31 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-09-22 18:44:16                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-09-25 11:35:38                               *
+ * @LastEditDate          : 2023-09-25 13:28:09                               *
  *****************************************************************************/
 
 /* SUMMARY
   * Imports
   * Services
   * Interfaces
+  * Create new project
 */
 
 /* Imports */
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Router } from "@angular/router";
 /***/
 
 /* Services */
 import { RecoveryService } from "src/app/services/recovery.service";
+import { RequestService } from "src/app/services/request.service";
+import { ToastService } from "src/app/services/toast.service";
+import { UserService } from "src/app/services/user.service";
 /***/
 
 /* Interfaces */
 import { IProjects } from "src/app/interfaces";
-import { Subscription } from "rxjs";
 /***/
 
 @Component({
@@ -33,7 +38,11 @@ export class ProjectsView implements OnInit, OnDestroy {
   public projects: IProjects[] | null = null;
   private subsciption: Subscription | null = null;
 
-  constructor(private recovery: RecoveryService) {
+  constructor(private router: Router,
+              private recovery: RecoveryService,
+              private api: RequestService,
+              private toast: ToastService,
+              private user: UserService) {
   }
 
   ngOnInit(): void {
@@ -43,4 +52,18 @@ export class ProjectsView implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.subsciption) this.subsciption.unsubscribe();
   }
+
+  /**
+  * Create new project
+  */
+  public createProject(): void {
+    this.api.post("api/projects", {name: "New project", description: "..."}, this.user.getUser()?.token)
+      .then((ret) => {
+        this.router.navigate([`/projects/${(ret as IProjects).id}`]);
+      }).catch((err) => {
+        console.error(err);
+        this.toast.print(`Login error >> ${err.message || err.statusText}`, "error");
+      });
+  }
+  /***/
 }
