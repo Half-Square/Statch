2b758ca9ccea89a326906ece733378c7d2a25bbc
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-09-27 12:02:52 +0200

    :lock: Prevent server crash on activities error generation, ignore error and continue

--- src/server/src/controllers/activities/activities.interceptor.ts
+++ src/server/src/controllers/activities/activities.interceptor.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-09-27 09:58:46                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-09-27 11:34:43                               *
+ * @LastEditDate          : 2023-09-27 12:01:59                               *
  *****************************************************************************/
 
 /* SUMMARY
@@ -32,21 +32,26 @@ export class ActivitiesInterceptor implements NestInterceptor {
   async intercept(
     context: ExecutionContext,
     next: CallHandler): Promise<Observable<any>> {
-    const req = context.switchToHttp().getRequest();
-    const controller = req.originalUrl.split("/")[2];
-    const user = jwt.verify(req.headers["x-token"], process.env.SALT);
-    let tmp: any;
-
-    if (req.method === "DELETE" || req.method === "PUT") { // Get item before request
-      tmp = await this.prisma[controller.substring(0, controller.length-1)].findUnique({where: {id: req.params["id"]}});
+    try {
+      const req = context.switchToHttp().getRequest();
+      const controller = req.originalUrl.split("/")[2];
+      const user = jwt.verify(req.headers["x-token"], process.env.SALT);
+      let tmp: any;
+  
+      if (req.method === "DELETE" || req.method === "PUT") { // Get item before request
+        tmp = await this.prisma[controller.substring(0, controller.length-1)].findUnique({where: {id: req.params["id"]}});
+      }
+  
+      return next.handle().pipe(map((data) => {
+        if (req.method === "POST") this.activities.handlePost(user, data, controller);
+        if (req.method === "PUT") this.activities.handlePut(user, tmp, data, controller);
+        if (req.method === "DELETE") this.activities.handleDelete(user, {id: tmp.id, name: tmp.name}, controller);
+  
+        return data;
+      }));
+    } catch (err) {
+      console.log(err);
+      return next.handle();
     }
-
-    return next.handle().pipe(map((data) => {
-      if (req.method === "POST") this.activities.handlePost(user, data, controller);
-      if (req.method === "PUT") this.activities.handlePut(user, tmp, data, controller);
-      if (req.method === "DELETE") this.activities.handleDelete(user, {id: tmp.id, name: tmp.name}, controller);
-
-      return data;
-    }));
   }
 }
