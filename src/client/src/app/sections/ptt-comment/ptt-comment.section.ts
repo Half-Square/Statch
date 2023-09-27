ffb4eb2ad5b447c79004e32ab0c05fc9425c0258
0K00 <qdouvillez@gmail.com>
2023-09-27 18:33:55 +0200

    :sparkles: Add ptt comment section

--- src/client/src/app/sections/ptt-comment/ptt-comment.section.ts
+++ src/client/src/app/sections/ptt-comment/ptt-comment.section.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+/*****************************************************************************
+ * @Author                : 0K00<qdouvillez@gmail.com>                       *
+ * @CreatedDate           : 2023-09-27 15:26:28                              *
+ * @LastEditors           : 0K00<qdouvillez@gmail.com>                       *
+ * @LastEditDate          : 2023-09-27 15:26:28                              *
+ ****************************************************************************/
+
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "section-ptt-comment",
@@ -6,5 +13,6 @@ import { Component } from "@angular/core";
   styleUrls: ["./ptt-comment.section.scss"]
 })
 export class PttCommentSection {
-
+  @Input()
+    comments: any = [];
 }
