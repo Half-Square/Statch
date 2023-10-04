1e65d144a93c49c2ae39d2f418fa069070f67717
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-10-04 13:27:39 +0200

    :poop: Static url for client env

--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -2,11 +2,11 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>        *
  * @CreatedDate           : 2023-09-20 16:39:57                              *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>        *
- * @LastEditDate          : 2023-10-04 11:56:33                              *
+ * @LastEditDate          : 2023-10-04 12:50:50                              *
  ****************************************************************************/
 
 export const environment = {
   production: true,
-  serverUrl: "http://localhost:8420",
-  socketUrl: "http://localhost:8421"
+  serverUrl: "http://hs-nas.local:8420",
+  socketUrl: "http://hs-nas.local:8421"
 };
