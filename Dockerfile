ce0ab629268537f31bb1f9ee7d24476b7a3276d0
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-10-04 12:21:24 +0200

    :construction_worker: Fix prod port

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM node:18
-WORKDIR /usr/src/app
+WORKDIR /usr/statch
 COPY ./dist/ .
 RUN npm i --omit=dev
 RUN npx prisma migrate deploy
 RUN npx prisma generate
-EXPOSE 5000
-EXPOSE 5001
+EXPOSE 8420
+EXPOSE 8421
 CMD ["node", "main.js"]
-VOLUME ["./prisma/databases"]
+VOLUME ["/prisma/databases"]
--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -1,12 +1,12 @@
-/******************************************************************************
- * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @CreatedDate           : 2023-09-20 16:39:57                               *
- * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-09-20 16:46:11                               *
- *****************************************************************************/
+/*****************************************************************************
+ * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>        *
+ * @CreatedDate           : 2023-09-20 16:39:57                              *
+ * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>        *
+ * @LastEditDate          : 2023-10-04 11:56:33                              *
+ ****************************************************************************/
 
 export const environment = {
   production: true,
-  serverUrl: "http://localhost:5000",
-  socketUrl: "http://localhost:5001"
+  serverUrl: "http://localhost:8420",
+  socketUrl: "http://localhost:8421"
 };
