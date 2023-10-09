00d6e37b6ac19b16a215976f28cc9d8b49f9d0d5
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-10-09 12:05:08 +0200

    :construction_worker: Implement https options for production

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>         *
  * @CreatedDate           : 2023-05-30 12:09:38                               *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>         *
- * @LastEditDate          : 2023-10-05 20:50:41                               *
+ * @LastEditDate          : 2023-10-09 10:35:32                               *
  *****************************************************************************/
 
 /* SUMMARY
@@ -13,10 +13,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
+import * as fs from "fs";
+import { resolve } from "path";
 /***/
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = process.env.PROD === "true" ? {
+    key: fs.readFileSync(resolve("certs/privkey.pem")),
+    cert: fs.readFileSync(resolve("certs/cert.pem"))
+  } : undefined;
+
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.enableCors();
 
