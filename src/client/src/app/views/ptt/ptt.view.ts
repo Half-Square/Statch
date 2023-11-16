f7b5ef23eb81d0cd69981880e234e1f9a127ee5a
Jbristhuille <jean-baptiste@halfsquare.fr>
2023-11-16 15:31:45 +0100

    :bug: Fix child advancement bar on version filter

--- src/client/src/app/views/ptt/ptt.view.ts
+++ src/client/src/app/views/ptt/ptt.view.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jean-baptiste@halfsquare.fr>        *
  * @CreatedDate           : 2023-09-30 15:55:46                              *
  * @LastEditors           : Jbristhuille<jean-baptiste@halfsquare.fr>        *
- * @LastEditDate          : 2023-11-14 10:37:14                              *
+ * @LastEditDate          : 2023-11-16 15:30:49                              *
  ****************************************************************************/
 
 /* SUMMARY
@@ -53,6 +53,7 @@ export class PttView implements OnInit, OnDestroy {
   public _ = _;
   public versionFilters: IVersions[];
   public root: IProjects;
+  public advancement: number = 0;
 
   private subscriptions: Subscription[] = [];
   private routeListener: Subscription;
@@ -78,7 +79,6 @@ export class PttView implements OnInit, OnDestroy {
           this.subscriptions.map((sub) => sub.unsubscribe()); // Clear old subscriptions
           this.subscriptions = [
             this.recovery.getSingle(this.type, this.id).subscribe((el) => { // Get current item
-              console.log(el, !el);
               if (!el) this.router.navigate(["/not-found"]);
               else this.item = el;
             }),
@@ -134,6 +134,8 @@ export class PttView implements OnInit, OnDestroy {
       this.childs = _.filter(el, (c) => {
         return this.id === (this.childType == "tasks" ? (c as ITasks).projectId : (c as ITickets).taskId);
       });
+
+      this.setAdvancement();
     });
   }
   /***/
@@ -180,14 +182,25 @@ export class PttView implements OnInit, OnDestroy {
 
   /**
   * Set item advancement
-  * @return - Percent value of child with done state
   */
-  public setAdvancement(): number {
-    let length = this.childs.length;
-    let completed = (_.countBy(this.childs, {status: "done"}) as {true: number | undefined}).true || 0;
+  public setAdvancement(): void {
+    let childs = _.filter(this.childs, (el: ITasks | ITickets) => { // Filter child by selected version
+      if (this.versionFilters.length > 0) {
+        return el.targetVersionId ?
+          _.find(this.versionFilters, {id: el.targetVersionId})
+          :
+          false;
+      } else return true;
+    });
+    let length = childs.length;
+
+    let completed = (_.countBy(childs, (el: ITasks | ITickets) => {
+      let status = el.status;
+      return status == "done" || status == "reject" || status == "wait";
+    }) as {true: number | undefined}).true || 0;
     let res = Math.floor(completed * 100 / length);
 
-    return !_.isNaN(res) ? res : 0;
+    this.advancement = !_.isNaN(res) ? res : 0;
   }
   /***/
 
