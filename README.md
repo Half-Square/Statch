79812d8928d16aeec363d3679b94f1a4276e423c
Jbristhuille <jean-baptiste@halfsquare.fr>
2024-01-30 11:14:53 +0100

    :bug: Fix database compatibility

--- README.md
+++ README.md
@@ -105,9 +105,9 @@ To get started with the Statch, you'll need to have the following software insta
 
 3. To install for development purpose
 
-  ```
+   ```
     npm run setup:dev
-  ```
+   ```
    
 ### Run project
 
--- src/server/prisma/migrations/20240130100628_reinit/migration.sql
+++ src/server/prisma/migrations/20240130100628_reinit/migration.sql
@@ -1,3 +1,11 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "permissions" TEXT NOT NULL,
+    "default" BOOLEAN NOT NULL
+);
+
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_User" (
@@ -11,9 +19,12 @@ CREATE TABLE "new_User" (
     "roleId" TEXT,
     CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-INSERT INTO "new_User" ("email", "id", "isAdmin", "name", "password", "picture", "roleId", "validate") SELECT "email", "id", "isAdmin", "name", "password", "picture", "roleId", "validate" FROM "User";
+INSERT INTO "new_User" ("email", "id", "isAdmin", "name", "password", "picture", "validate") SELECT "email", "id", "isAdmin", "name", "password", "picture", "validate" FROM "User";
 DROP TABLE "User";
 ALTER TABLE "new_User" RENAME TO "User";
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
